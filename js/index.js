7583fdf69d52db8eaf26719928854757c18b9c8a
Mass Horse <1150236238@qq.com>
2019-02-26 22:00:36 +0800

    blog

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 var oH1 = document.querySelector('h1');
-var flag = true;
-oH1.onclick = function(){
-	if(flag){
-		this.style.color = 'green';
-	}else{
-		this.style.color = 'red';
-	}
-	flag = !flag;
+var oBtn = document.querySelector('button');
+
+function color16(){
+	var r = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	var color = '#'+r.toString(16)+g.toString(16)+b.toString(16);
+	return color;
 }
+
+var timer = setInterval(function(){
+	oH1.style.color = color16();
+}, 500);
